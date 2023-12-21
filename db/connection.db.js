e5a3f27304bee96999a88d22b3dfd0acf0366bfd
nikhil0450 <nikhilkulkarni2208@gmail.com>
2023-12-22 01:48:45 +0530

    uri

--- db/connection.db.js
+++ db/connection.db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
+
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to the database...");
   } catch (error) {
     console.log("Error: ", error);
